17c99e5c7b20c4cf3acfdaaa2f91becb3572e30b
HebaZaki <h_borhan@hotmail.com>
2024-05-26 12:49:54 +0300

    update init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,12 @@
 """Viewa initalozation file"""
+
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 
 app_views = Blueprint('views', __name__, url_prefix="/api/v1")
 
+from api.v1.views.index import *
+from api.v1.views.states import *
 # from api.v1.views.cities import *
 # from api.v1.views.amenities import *
 # from api.v1.views.places import *
