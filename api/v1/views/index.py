eec076fcb4cf539b2efc9ce74e78d0981583a231
Heba <hebamagde@gmail.com>
2024-05-26 05:30:15 +0300

    restore

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,14 +2,8 @@
 """index of views"""
 
 from api.v1.views import app_views
-from flask import jsonify, make_response
+from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -18,11 +12,15 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'])
 def stats():
     """Retrieves the number of each object by type"""
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
-
-    obj_count = {key: storage.count(value) for key, value in classes.items()}
-    return make_response(jsonify(obj_count))
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
