cf2416c8647504716b16daaafd7f46c00b7ffa96
Heba <hebamagde@gmail.com>
2024-05-27 05:38:38 +0300

    try

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,8 +9,24 @@ from api.v1.views import app_views
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Get list of all State objects"""
-    states = list(storage.all(State).values())
+    states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Creates a State object"""
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
+        abort(400, description="Not a JSON")
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route(
@@ -35,31 +51,20 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
-    """Creates a State object"""
-    if not request.json:
-        abort(400, description="Not a JSON")
-    if 'name' not in request.json:
-        abort(400, description="Missing name")
-    data = request.get_json()
-    new_state = State(**data)
-    storage.new(new_state)
-    storage.save()
-    return jsonify(new_state.to_dict()), 201
-
-
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object by its ID"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if not request.json:
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
         abort(400, description="Not a JSON")
-    data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
-    storage.save()
+    state.save()
     return jsonify(state.to_dict()), 200
