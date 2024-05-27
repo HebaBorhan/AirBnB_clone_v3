1a87726c456c73435dbe665cd97f04e842b9e7e7
Heba <hebamagde@gmail.com>
2024-05-27 05:49:37 +0300

    try

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -16,13 +16,17 @@ def get_amenities():
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
     """Creates an Amenity object"""
-    if not request.json:
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
         abort(400, description="Not a JSON")
-    data = request.get_json()
     if 'name' not in data:
         abort(400, description="Missing name")
     new_amenity = Amenity(**data)
-    new_amenity.save()
+    storage.new(new_amenity)
+    storage.save()
     return jsonify(new_amenity.to_dict()), 201
 
 
@@ -55,11 +59,14 @@ def update_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    if not request.json:
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
         abort(400, description="Not a JSON")
-    data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(amenity, key, value)
-    amenity.save()
+    storage.save()
     return jsonify(amenity.to_dict()), 200
