6d85bbaedf2da9285c256d77065d1ab683af2ac6
Heba <hebamagde@gmail.com>
2024-05-27 05:55:11 +0300

    update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -18,6 +18,33 @@ def get_places(city_id):
     return jsonify([place.to_dict() for place in city.places])
 
 
+@app_views.route(
+        '/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+def create_place(city_id):
+    """Creates a Place object"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
+        abort(400, description="Not a JSON")
+    if 'user_id' not in data:
+        abort(400, description="Missing user_id")
+    user = storage.get(User, data['user_id'])
+    if user is None:
+        abort(404)
+    if 'name' not in data:
+        abort(400, description="Missing name")
+    data['city_id'] = city_id
+    new_place = Place(**data)
+    storage.new(new_place)
+    storage.save()
+    return jsonify(new_place.to_dict()), 201
+
+
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     """Get Place object by its ID"""
@@ -39,39 +66,18 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route(
-        '/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
-def create_place(city_id):
-    """Creates a Place object"""
-    city = storage.get(City, city_id)
-    if city is None:
-        abort(404)
-    if not request.json:
-        abort(400, description="Not a JSON")
-    data = request.get_json()
-    if 'user_id' not in data:
-        abort(400, description="Missing user_id")
-    user = storage.get(User, data['user_id'])
-    if user is None:
-        abort(404)
-    if 'name' not in data:
-        abort(400, description="Missing name")
-    data['city_id'] = city_id
-    new_place = Place(**data)
-    storage.new(new_place)
-    storage.save()
-    return jsonify(new_place.to_dict()), 201
-
-
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object by its ID"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    if not request.json:
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
         abort(400, description="Not a JSON")
-    data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
             setattr(place, key, value)
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -13,6 +13,25 @@ def get_users():
     return jsonify([user.to_dict() for user in users])
 
 
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a User object"""
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
+        abort(400, description="Not a JSON")
+    if 'email' not in data:
+        abort(400, description="Missing email")
+    if 'password' not in data:
+        abort(400, description="Missing password")
+    new_user = User(**data)
+    storage.new(new_user)
+    storage.save()
+    return jsonify(new_user.to_dict()), 201
+
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """Get User object by its ID"""
@@ -33,31 +52,18 @@ def delete_user(user_id):
     return jsonify({}), 200
 
 
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
-def create_user():
-    """Creates a User object"""
-    if not request.json:
-        abort(400, description="Not a JSON")
-    data = request.get_json()
-    if 'email' not in data:
-        abort(400, description="Missing email")
-    if 'password' not in data:
-        abort(400, description="Missing password")
-    new_user = User(**data)
-    storage.new(new_user)
-    storage.save()
-    return jsonify(new_user.to_dict()), 201
-
-
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """Updates a User object by its ID"""
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
-    if not request.json:
+    try:
+        data = request.get_json()
+    except Exception:
+        data = None
+    if not data:
         abort(400, description="Not a JSON")
-    data = request.get_json()
     for key, value in data.items():
         if key not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(user, key, value)
