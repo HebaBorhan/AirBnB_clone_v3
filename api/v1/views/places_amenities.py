001a8438514e830a71cfff4442ba580a06841ff4
Heba <hebamagde@gmail.com>
2024-05-27 07:39:25 +0300

    update

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -15,7 +15,8 @@ def get_place_amenities(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    return jsonify([amenity.to_dict() for amenity in place.amenities])
+    return jsonify([amenity.to_dict()
+                    for amenity in place.amenities])
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
